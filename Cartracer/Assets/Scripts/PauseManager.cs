2e3566af5d5c8cf28f89e0974c3a1d005de10103
Joel Matthys <joel@matthysmusic.com>
2025-04-28 12:47:14 -0500

    Improve UI

--- Cartracer/Assets/Scripts/PauseManager.cs
+++ Cartracer/Assets/Scripts/PauseManager.cs
@@ -2,6 +2,24 @@ using UnityEngine;
 
 public class PauseManager : MonoBehaviour
 {
+    private bool isPaused = false;
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                isPaused = true;
+                OnPausePressed();
+            }
+            else
+            {
+                isPaused = false;
+                OnResumePressed();
+            }
+        }
+    }
+
     public void OnQuitPressed()
     {
         Debug.Log("Quit");
